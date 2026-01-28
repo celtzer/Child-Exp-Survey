db8a2efdb2301a2b58df7c5646b187cbf56ca381
jaxsing <jaxsing@udel.edu>
2026-01-28 14:05:22 -0500

    added checkbox and cleaned up some code

--- script-files/main.js
+++ script-files/main.js
@@ -1,68 +1,16 @@
-const form = document.getElementById("ACESxBCES");
-const steps = Array.from(document.querySelectorAll(".step"));
-const progress = document.getElementById("progress");
 
-const backBtn = document.getElementById("backBtn");
-const nextBtn = document.getElementById("nextBtn");
-const submitBtn = document.getElementById("submitBtn");
+const startBtn = document.getElementById("startBtn");
+const agree1 = document.getElementById("agree1")
+const agree2 = document.getElementById("agree2")
 
-let current = 0;
-
-function showStep(i) {
-    steps.forEach((s, idx) => s.classList.toggle("active", idx === i));
-
-    // Progress text
-    progress.textContent = `Question ${i + 1} of ${steps.length}`;
-
-    // Button visibility
-    backBtn.style.display = i === 0 ? "none" : "inline-block";
-    nextBtn.style.display = i === steps.length - 1 ? "none" : "inline-block";
-    submitBtn.style.display = i === steps.length - 1 ? "inline-block" : "none";
-}
-function stepIsValid(i) {
-    // Validate only inputs inside this step
-    const inputs = steps[i].querySelectorAll("input, select, textarea");
-    for (const el of inputs) {
-        if (!el.checkValidity()) return false;
-    }
-    return true;
+function update() {
+    startBtn.disabled = !(agree1.checked && agree2.checked);
 }
 
-nextBtn.addEventListener("click", () => {
-    if (!stepIsValid(current)) {
-        // Triggers the browser's built-in validation message
-        const firstInvalid = steps[current].querySelector("input:invalid, select:invalid, textarea:invalid");
-        firstInvalid?.reportValidity();
-        return;
-    }
-    current++;
-    showStep(current);
+agree1.addEventListener("change", update);
+agree2.addEventListener("change", update);
+
+startBtn.addEventListener("click", () => {
+    window.location.href = "quiz.html";
 });
-
-backBtn.addEventListener("click", () => {
-    current--;
-    showStep(current);
-});
-form.addEventListener("submit", (e) => handleSurveySubmit(e));
-function handleSurveySubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(form);
-    let ACES_sum = 0;
-    let BCES_sum = 0;
-    for (const [name, value] of formData.entries()) {
-        if(name[0] === "B"){
-            if(value === '1')BCES_sum++;
-        }else{
-            if(value === '1')ACES_sum++;
-        }
-    }
-    localStorage.setItem("results", JSON.stringify({
-        ACES_sum,
-        BCES_sum
-    }));
-
-    // Go to results page
-    window.location.href = "results.html";
-
-}
-showStep(0);
+update();
